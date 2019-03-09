d7894037a586b078d101f45864efbeab6010245e
Ed <bruzzi.eduardo@gmail.com>
2019-03-09 20:00:43 +1300

    first pusblished version

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,19 +1,19 @@
-function debounce(fn, wait) {
-  var timeout;
-  return function () {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      fn.apply(this, arguments);
-    }, (wait || 1));
-  };
-}
+// function debounce(fn, wait) {
+//   var timeout;
+//   return function () {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(function () {
+//       fn.apply(this, arguments);
+//     }, (wait || 1));
+//   };
+// }
 
-window.addEventListener('resize', debounce(function () {
+// window.addEventListener('resize', debounce(function () {
 
-    var vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', vh + 'px' );
+//     var vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty('--vh', vh + 'px' );
 
-}, 500));
+// }, 500));
 
 (function() {
 
@@ -47,44 +47,47 @@ window.addEventListener('resize', debounce(function () {
 
   (function($) {
 
-    // init controller
-    var parallaxController = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+    if ($(".parallaxParent")[0]){
+      
+      // init controller
+      var parallaxController = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
-    // build scenes
-    new ScrollMagic.Scene({triggerElement: "#hero.parallaxParent"})
-            .setTween("#hero.parallaxParent > .parallax-img", {y: "80%", ease: Linear.easeNone})
-            //.addIndicators()
-            .addTo(parallaxController);
+      // build scenes
+      new ScrollMagic.Scene({triggerElement: "#hero.parallaxParent"})
+              .setTween("#hero.parallaxParent > .parallax-img", {y: "80%", ease: Linear.easeNone})
+              //.addIndicators()
+              .setClassToggle('#hero', 'is-active')
+              .addTo(parallaxController);
 
-    $('.content-copy, .hero-content').find('h1,h2,h3,li,p,span').each(function() {
-      $(this).html($(this).html().replace(/\s([^\s<]{0,11})\s*$/,'&nbsp;$1'));
-    });
+      // $('.content-copy, .hero-content').find('h1,h2,h3,li,p,span').each(function() {
+      //   $(this).html($(this).html().replace(/\s([^\s<]{0,11})\s*$/,'&nbsp;$1'));
+      // });
 
-    // init controller
-    var controller = new ScrollMagic.Controller();
+      // init controller
+      var controller = new ScrollMagic.Controller();
 
-    $('.scrollable').each(function(){
-      var currentShow = this;
+      $('.scrollable').each(function(){
+        var currentShow = this;
 
-      var sceneShow = new new ScrollMagic.Scene({triggerElement: currentShow, triggerHook: 'onEnter'})
-      .setClassToggle(currentShow, 'show')
-      .on('leave', function(e) {
-         // if (currentShow.classList.contains('sun')) {
-         //  sunTl.time(0);
-         // } else if (currentShow.classList.contains('odor')) {
-         //  odorTl.time(0);
-         // } else if (currentShow.classList.contains('sus')) {
-         //  tl.time(0);
-         // }
-      })
-      // .addIndicators()
-      .addTo(controller);
-    }); 
+        var sceneShow = new new ScrollMagic.Scene({triggerElement: currentShow, triggerHook: 'onEnter'})
+        .setClassToggle(currentShow, 'show')
+        .on('leave', function(e) {
+           // if (currentShow.classList.contains('sun')) {
+           //  sunTl.time(0);
+           // } else if (currentShow.classList.contains('odor')) {
+           //  odorTl.time(0);
+           // } else if (currentShow.classList.contains('sus')) {
+           //  tl.time(0);
+           // }
+        })
+        // .addIndicators()
+        .addTo(controller);
+      }); 
 
-    }(jQuery));
+    }
+    
+
+  }(jQuery));
 
 }).call(this);
-
-
-
 
